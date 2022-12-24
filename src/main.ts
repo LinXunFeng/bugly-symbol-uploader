ad316e5be9fbf390a332fd44606978704a1020bd
LinXunFeng <linxunfeng@yeah.net>
2022-12-24 20:01:26 +0800

    feat : 调整项目配置，初始化命令

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+#! /usr/bin/env node
 
-createApp(App).mount('#app')
+import figlet from 'figlet';
+import { Command } from 'commander';
+
+console.log(figlet.textSync("Bugly Symbol"));
+
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .description("Bugly符号表上传工具")
+  .option("-l, --ls [value]", "列出所有")
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.ls) {
+  console.log("ls命令");
+}
+
+if (options.mkdir) {
+  console.log("mkdir命令");
+
+}
