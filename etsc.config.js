957b2c741389e6cddb4323189367acf6260d2caf
LinXunFeng <linxunfeng@yeah.net>
2022-12-25 23:17:24 +0800

    update : 更新项目配置

--- etsc.config.js
+++ etsc.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   esbuild: {
-    minify: true
-  }
+    minify: false
+  },
+  // Postbuild hook
+  postbuild: async () => {
+    console.log("postbuild");
+    const cpy = (await import("cpy")).default;
+    await cpy(
+      [
+        "src/static/**/*.*", // Copy all .json files
+      ],
+      "dist/static"
+    );
+  },
 };
